28e668c07e6deb4a8327c84dce93371171383798
Dustin Davisson <dustin.davisson@gmail.com>
2022-08-07 20:29:06 -0700

    Further along main body, have save button all columns time based coloring.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 // get the current day and display in the jumbotron.
 
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
@@ -7,23 +8,25 @@ setInterval(function() {
 }, 1000);
 
 const mainPageRender = function() {
-  for (let x = 9; x<18; x++) {
+  for (let x = 7; x<19; x++) {
     let taskCol = $();
-    console.log(moment().format('H') > moment().hour(x).format('H'));
-    console.log(moment().hour(x).format('H'));
-
-    const timeCol = $('<div class="col-1 hour"></div>').text(moment().hour(x).format('h A'));
-    if (moment().hour(x).format('H') < moment().format('H')) {
-      taskCol = $('<div class="col-10 time-block past"></div>').text('test');
+    const timeCol = $('<div class="col-1 hour"></div>')
+        .text(moment().hour(x).format('h A'));
+    // Make sure to make the moment output a number!
+    if (Number(moment().hour(x).format('H')) < Number(moment().format('H'))) {
+      taskCol = $('<div class="col-10 time-block past"><textarea  id="Text' + x +'"></textarea></div>');
     } else if (moment().hour(x).format('H') === moment().format('H')) {
-      taskCol = $('<div class="col-10 time-block present"></div>').text('test');
+      taskCol = $('<div class="col-10 time-block present"><textarea class="form-control" id="Text' + x +'"></textarea></div>');
     } else {
-      taskCol = $('<div class="col-10 time-block future"></div>').text('test');
+      taskCol = $('<div class="col-10 time-block future"></div>');
     }
-    const addCol = $('<div class="col-1 saveBtn"></div>');
+    const addCol = $('<div class="col-1 saveBtn fa-save fa-lg fa" id="' + x + '"><a href="#"></a></div>');
+    $('.container').append($('<div class=row>')
+        .append(timeCol, taskCol, addCol));
+    addCol.on('click', function() {
 
-    $('.container').append($('<div class=row>').append(timeCol, taskCol, addCol));
+    });
   }
 };
 
-mainPageRender();
+setInterval(mainPageRender(), 60000);
